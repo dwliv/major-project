1c2cd276da7d3c3f74a3ce2f2e97de8b09bf5d72
dwliv <dinel.rw123@gmail.com>
2024-11-21 12:15:46 -0600

    added a start, and end scree, started to add platforms and blocks, gonna add coins and maybe enemies next work period and work on the design later. Added what I need and what's nice to have list

--- proposal.md
+++ proposal.md
@@ -1,12 +1,24 @@
 # Project Description
 
-Explain what I'm going to build here.
+- For right now what I'm thinking if building is a level of New Super Mario Bros for the Nintendo DS in 2006. Just a basic level from world one is what I will try to build
 
 ## Needs to have list
 
-- thing one
-- thing two
+- Player
+- Start Screen
+- Game Over Screen
+- Enemies
+- Blocks
+- Coin Counter
+- Graphics
+- Sound
+- Flagpole at the end
 
 ## Nice to have list
-- other thing
-- yet one more thing
+- Power-ups
+- Life Counter
+- -Time Limit 
+- Pipes
+- Star Coins
+- Check Point 
+- Mega Mushroom
--- sketch.js
+++ sketch.js
@@ -1,10 +1,20 @@
-// Project Title
-// Your Name
+//Final Project
+// Dinel Widyaratne
 // Date
-//
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let player;
+let platforms = [];
+let blocks = [];
+let coins = []; 
+let enemies = [];
+let groundLevel = 350;
+let gravity = 1;
+let collectedCoins = 0;
+let gameState = "start";
+let windowWidth = 800;
+let windowHeight = 500;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,6 +22,48 @@ function setup() {
 
 function draw() {
   background(220);
+  if (gameState === "start") {
+    showStartScreen();
+  }
+  else if (gameState === "run") {
+    runGame();
+  }
+  else if (gameState === "gameOver") {
+    gameOverScreen();
+  }
+}
 
-  circle(mouseX, mouseY, 100);
+function runGame() {
+  fill(0);
+  textSize(13);
+  text(`Coins: ${collectedCoins}`, 10, 20);
+
+  //Draw Platforms
+  for (let platform of platforms) {
+    platform.display();
+  }
+
+  //Draw Blocks
+  for (let block of blocks) {
+    blocks.display();
+  }
+}
+
+function showStartScreen() {
+  fill(0);
+  textSize(38);
+  textAlign(CENTER, CENTER);
+  text("New Super Mario Bros", width / 2, height / 2);
+  textSize(22);
+  text("Press Enter to Start", width / 2, height / 2);
+  textAlign(BOTTOM, CENTER);
+}
+
+function gameOverScreen() {
+  fill(0);
+  textSize(38);
+  textAlign(CENTER, CENTER);
+  text("Game Over", width/2, height/2);
+  textSize(22);
+  text("Please press R to restart and hopefully you'll get better and win!");
 }
